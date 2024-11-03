8a4e446baa7b82497c397ef6dfbbbfcc341776d6
kanomkway <157315857+kanomkway@users.noreply.github.com>
2024-11-03 09:34:47 +0000

    2

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:false}))
+app.use((req,res,next)=>{
+  res.setHeader("Access-Control-Allow-Origin", "*")
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Request-With, Content-Type, Accept, Authorization"
+  )
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET-POST-PUT-DELETE"
+  );
+  next();
+})
+app.use(express.json())
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
