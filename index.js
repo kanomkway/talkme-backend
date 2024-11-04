e4d39cca742fc1d772ea361c7e9c8aa0c487f3fb
kanomkway <157315857+kanomkway@users.noreply.github.com>
2024-11-04 06:53:36 +0000

    test

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const port = 5000
 const bodyParser = require('body-parser')
+var mysql = require('mysql');
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:false}))
@@ -19,8 +20,28 @@ app.use((req,res,next)=>{
 })
 app.use(express.json())
 
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "123456",
+  port: "33066",
+  database: "talkme",
+});
+
+con.connect(function(err){
+  if(err) throw err;
+});
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.get('/api/general',(req,res)=>{
+  con.query("SELECT * FROM tag_general",function(err,result,fields){
+    if(err) throw res.status(400).send("No products found");
+    console.log(result);
+    res.send(result);
+  });
 })
 
 app.listen(port, () => {
